9f9bd328fab840173dac62d2eff3b0643be44a0e
Victor Requena <svictoreq@gmail.com>
2018-11-07 16:30:17 -0400

    Add the "Already logged" screen

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciStepper from './UdaciStepper';
 import DateHeader from './DateHeader';
+import TextButton from './TextButton';
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -74,8 +76,41 @@ export default class AddEntry extends Component {
     // Clear local notification
   }
 
+  reset = () => {
+    const key = timeToString();
+
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+    }))
+
+    // Update Redux
+
+    // Route to home
+
+    // Update 'DB'
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (/*this.props.alreadyLogged*/ true) {
+      return (
+        <View>
+          <Ionicons
+            name={'md-happy'}
+            size={100}
+          />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      );
+    }
 
     return (
       <View>
