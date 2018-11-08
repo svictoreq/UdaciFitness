13b4f156e56b9ecab042014c93c9e8dc3eb05559
Victor Requena <svictoreq@gmail.com>
2018-11-08 16:09:43 -0400

    Add styles for increment/decrement buttons in both iOS and Android

--- components/UdaciStepper.js
+++ components/UdaciStepper.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { FontAwesome, Entypo } from '@expo/vector-icons';
+import { View, Text, TouchableHighlight, TouchableOpacity, StyleSheet, Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { white, purple } from '../utils/colors';
 
 export default function UdaciStepper ({ max, unit, step, value, onIncrement, onDecrement }) {
+  const minusName = Platform.OS === 'ios' ? 'ios-remove' : 'md-remove';
+  const plusName = Platform.OS === 'ios' ? 'ios-add' : 'md-add';
+  const stepper = Platform.OS === 'ios'
+    ? (<View style={[styles.iosBtnContainer]}>
+        <TouchableHighlight onPress={onDecrement} underlayColor={purple} style={[styles.iosBtn]}>
+          <Ionicons name={minusName} size={22} color={purple} />
+        </TouchableHighlight>
+        <TouchableHighlight onPress={onIncrement} underlayColor={purple} style={[styles.iosBtn, styles.iosBtnLast]}>
+          <Ionicons name={plusName} size={22} color={purple} />
+        </TouchableHighlight>
+        </View>)
+    : (<View style={{flexDirection: 'row'}}>
+          <TouchableOpacity onPress={onDecrement} style={[styles.androidBtn]}>
+            <Ionicons name={minusName} size={22} color={white} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={onIncrement} style={[styles.androidBtn]}>
+            <Ionicons name={plusName} size={22} color={white} />
+          </TouchableOpacity>
+        </View>)
   return (
-    <View>
-      <TouchableOpacity onPress={onDecrement}>
-        <FontAwesome name={'minus'} size={30} color={'black'} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={onIncrement}>
-        <FontAwesome name={'plus'} size={30} color={'black'} />
-      </TouchableOpacity>
+    <View style={[styles.row, { justifyContent: 'space-between' }]}>
+      {stepper}
       <View>
         <Text>{value}</Text>
         <Text>{unit}</Text>
@@ -18,3 +33,43 @@ export default function UdaciStepper ({ max, unit, step, value, onIncrement, onD
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  iosBtnContainer: {
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: purple,
+    borderRadius: 5,
+  },
+  iosBtn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    height: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  iosBtnLast: {
+    borderColor: purple,
+    borderLeftWidth: 1,
+  },
+  androidBtn: {
+    margin: 3,
+    backgroundColor: purple,
+    height: 36,
+    width: 36,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+  metrics: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
